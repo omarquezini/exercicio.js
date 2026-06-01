7c1102806f3a8d8ec0f08f5f8a5a1cb7c890d242
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2026-06-01 17:42:46 +0000

    lisata de tabua.

---
+++ listaVick.js
@@ -0,0 +1,7 @@
+let convidados = [julho, julia, julinho, wellberson];
+
+let Nconvidados = [julio];
+
+if (Nconvidados === convidados){
+    console.log("")
+}
--- tabua.js
+++ tabua.js
@@ -1,6 +1,6 @@
 let numero = 7;
 
-for (let i = 0; i < 10; i++){
+for (let i = 0; i < 11; i++){
     
     console.log(`${numero} X ${i} = ${numero * i}`);
 
