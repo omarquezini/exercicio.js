3bb27eb2d5539d594e1cfebc53a3e1dd662af0ba
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2026-06-01 17:16:35 +0000

    fazendo midia.

---
+++ midia.js
@@ -0,0 +1,20 @@
+let nota1 = 5.0;
+
+let nota2 = 5.0;
+
+let nota3 = 5.0;
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+console.log(`sua media é ${media}`)
+
+if (media >= 6){
+    console.log("Aprovado, agora cai fora.")
+}
+else if (media >= 4 && media < 6){
+    console.log("foi quase... sua anta.")
+
+}
+else{
+    console.log("REPROVADO. foi um despraser servir com o senhor, seu miseravel")
+}
