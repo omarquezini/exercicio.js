807de985c2ade4975e8ae69c5bd9c8011bf25216
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2026-06-01 17:35:43 +0000

    radar de tabuas gays.

---
+++ radarGay.js
@@ -0,0 +1,8 @@
+let Km = 80;
+
+if (Km > 80){
+    console.log("ta indo dá aonde com tanta pressa.")
+}
+else{
+    console.log("isso... bem assim que se faz, gostosa *piscada*.")
+}
---
+++ tabua.js
@@ -0,0 +1,7 @@
+let numero = 7;
+
+for (let i = 0; i < 10; i++){
+    
+    console.log(`${numero} X ${i} = ${numero * i}`);
+
+}
